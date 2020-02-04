6b0c269de92711e6bd799927dff4204d442d336f
dancorrea86 <danielmacielcorrea@gmail.com>
2020-02-03 21:18:13 -0300

    commit

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const Joi = require('@hapi/joi');
 const express = require('express');
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 const courses = [
-  { id: 1, name: 'course1'},
-  { id: 2, name: 'course2'},
-  { id: 3, name: 'course3'}
-]
+  { id: 1, name: 'course1' },
+  { id: 2, name: 'course2' },
+  { id: 3, name: 'course3' },
+];
+
 app.get('/', (req, res) => {
   res.send('Hello World!!!');
 });
@@ -27,10 +28,14 @@ app.post('/api/courses', (req, res) => {
   const course = {
     id: courses.length + 1,
     name: req.body.name
-  }
-  courses.push(course);
-  res.send(course)
+  };
+  courses.push(course)
+  res.send(courses)
 });
+
+
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
