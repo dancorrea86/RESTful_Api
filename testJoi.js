b57898a0008b501896bbfed91447b29f742d13e2
dancorrea86 <danielmacielcorrea@gmail.com>
2020-02-06 22:05:52 -0300

    commit

--- testJoi.js
+++ testJoi.js
@@ -2,15 +2,16 @@ const Joi = require('@hapi/joi');
 console.log(Joi.version)
 
 const schema = Joi.object({
-  
+  name: Joi.string().min(3).required(),
   username: Joi.string().required(),
   email: Joi.string().email().required()
 });
 
+let name = 'Roger'
 let username = 'Roger Brown';
 let email = 'roger@example.com';
 
-let data = { username, email };
+let data = { name, username, email };
 
 const validation = schema.validate(data);
 
