1cc99ab482c41c3c52422b61b8582e05d53689ae
dancorrea86 <danielmacielcorrea@gmail.com>
2020-02-10 21:50:22 -0300

    commit

--- index_solo.js
+++ index_solo.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser')
 
-app.use(express.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 app.post('/', (req, res) => {
-  const value = req.body.name;
+  const value = req.body.username;
   console.log(value)
+  res.send('Obrigado por participar');
 });
 
 app.listen(3000, 10, () => { console.log('ola') });
